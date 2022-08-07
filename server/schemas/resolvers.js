7361b3379175c737bb38523c7baa8424673f4578
RubenCastroCoding <RubenCastroCoding@gmail.com>
2022-08-07 14:47:44 -0400

    working on reslovers and getting the server back up because it was not running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,59 +1,15 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Estimate, Category } = require('../models');
+const { User, Estimate } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user.id).populate({
-          path: 'orders.estimates',
-          populate: 'estimates',
-        });
-
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
-        return user;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-  
-  },
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return User.findByIdAndUpdate(context.user.id, args, {
-          new: true,
-        });
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    },
-  },
+    Estimates: async () => {
+      // Get and return all documents from the users collection
+      return await Estimate.find({});
+    }
+  }
 };
 
 module.exports = resolvers;
+
