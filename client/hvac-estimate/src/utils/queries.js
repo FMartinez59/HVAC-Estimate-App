8b2d1399a2fdfc923282b7b14ee9d6ffa57f9da1
gretchesketch <gretchenjensen92@gmail.com>
2022-08-09 18:49:59 -0400

    utils

---
+++ client/hvac-estimate/src/utils/queries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PROFILES = gql`
+  query allProfiles {
+    profiles {
+      _id
+      name
+      estimate
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      estimate
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      name
+      estimate
+    }
+  }
+`;
