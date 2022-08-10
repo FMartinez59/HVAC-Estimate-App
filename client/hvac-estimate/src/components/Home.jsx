1c698367fff0726a936e6a1a978eddb630c308a8
Sahil Patel <sahildepatel@gmail.com>
2022-08-09 21:39:51 -0400

    Revamping form for estimate getter thing

--- client/hvac-estimate/src/components/Home.jsx
+++ client/hvac-estimate/src/components/Home.jsx
@@ -1,27 +1,28 @@
-import React from "react";
 // import { HiArrowNarrowRight } from "react-icons/hi";
 import Popup from "./Popup";
-import { useState } from 'react';
+import React, { useState } from 'react';
 const Home = () => {
   
-  const [buttonPopup, setButtonPopup] = useState(false);
+
+  const [showForm, setShowForm] = useState(false);
+  const handleOnClose = () => setShowForm(false);
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
-      {/* container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        
-        <h1 className="text-[#8892b0] py-4 max-w[700px]">
-        Already know what you are looking for?
-        </h1>
-
-        <div className="text-white group border-2 px-6 py-3 my-2 flex items-center ">
-          <button onClick={() => setButtonPopup(true)}>Get a free estimate here</button>
-            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          
-            </Popup>
+      {/* container */}    
+    <h1 className="text-[#8892b0] py-4 max-w[700px]">
+    Already know what you are looking for?
+    </h1>
+    <div className="bg-blue-400 bg-opacity-30">
+      <div className="max-w-3xl mx-auto">
+        <div className="text-center py-3">
+          <button onClick={() => setShowForm(true)} className="bg-red-400 text-white px-3 py-2 rounded hover:scale-95 transition text-xl">
+            Get A free Estimate Now
+          </button>
         </div>
       </div>
     </div>
+    <Popup onCLose={handleOnClose} visible={showForm}/>
+  </div>
   );
 };
 
--- client/hvac-estimate/src/components/Popup.jsx
+++ client/hvac-estimate/src/components/Popup.jsx
@@ -1,58 +1,77 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
 
-function Popup(props) {
-    return (props.trigger) ? (
-      <div className='popup'>
-          <div className='popup_inner'>
-              <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
-              {props.children}
-          </div>   
-          <form class="w-full max-w-lg">
-  
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <div class=" px-3">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="squareFeet">
-          What is the square footage of your house?
-        </label>
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="sqft" type="input" placeholder="2700"></input>
-        <p class="text-gray-600 text-xs strong">sq ft</p>
+export default function Popup({ visible, onClose }) {
+  const handleOnClose = (e) => {
+    if(e.target.id === "estimate") onClose();
+  }
+  if (!visible) return null;
+  return (
+    <div id="estimate"
+    onClick={handleOnClose}
+    className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+      <div className="bg-white p-56 rounded w-200">
+      <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-s font-bold mb-0" for="system">
+        What type of System do you have?
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="system">
+          <option>Furnace</option>
+          <option>Air Handler</option>
+          
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
       </div>
     </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-        How many tons is your current system?
-        </label>
-        <div class="relative">
-          <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="tons">
-            <option>2.0</option>
-            <option>3.0</option>
-            <option>4.0</option>
-            <option>5.0</option>
-          </select>
-          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-          </div>
+      <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-s font-bold mb-0 mt-4" for="grid-state">
+        How many tons is your System?
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>2.0 Tons</option>
+          <option>3.0 Tons</option>
+          <option>4.0 Tons</option>
+          <option>5.0 Tons</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
       </div>
-      <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-8" for="cabinets">
-          What cabinet is your system?
-        </label>
-        <div class="relative">
-          <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-4 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="type">
-            <option>Air Handler</option>
-            <option>Furnace</option>
-          </select>
-          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-          </div>
+    </div>
+   
+    <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-s font-bold mb-0 mt-4" for="grid-state">
+        What effeciency are you looking for?
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>14 seer</option>
+          <option>16 seer</option>
+          
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
       </div>
+    </div>
+    <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-s font-bold mb-0 mt-4" for="grid-state">
+        What is square footage of your house?
+      </label>
+      <div class="relative">
+        <input class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" placeholder="2700">
+        </input>
+        <p class="block uppercase tracking-wide text-gray-700 text-s font-bold mb-2">SQ FT</p>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-blue-700">
+          
+        </div>
+      </div>
+    </div>
     
-  </form>
-  </div>
-) :  "";
+      </div>
+    </div>
+  );
 }
-
-export default Popup
