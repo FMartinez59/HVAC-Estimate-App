aa1c09a4ed41984cd82a39fc705d13f686002595
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-08-10 19:33:13 -0400

    fixed seeding errors and db seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,15 @@
-const db = require('../config/connection');
-const { Estimate} = require('../models');
+const db = require("../config/connection");
+const { Estimate } = require("../models");
+const estimateSeed = require("./estimateData.json");
 
-const estimateSeed = require('./estimateData.json');
+db.once("open", async () => {
+  try {
+    await Estimate.deleteMany({});
+    await Estimate.create(estimateSeed);
 
-db.once('open', async () => {
- await Estimate.deleteMany({});
- await Estimate.create(thoughtSeeds);
-
-  console.log('estimateDataSeeded!');
-  process.exit(0);
+    console.log("estimateDataSeeded!");
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
 });
