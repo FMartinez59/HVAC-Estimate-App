814e7dc419da096bb85595c3d2c61fb3857ddd79
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-08-11 19:43:43 -0400

    working on getting client to render

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-  ApolloClient,
-  InMemoryCache,
+  // ApolloClient,
+  // InMemoryCache,
   ApolloProvider,
-  createHttpLink,
+  //createHttpLink,
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+//import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './pages/Profile';
@@ -13,26 +13,26 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
