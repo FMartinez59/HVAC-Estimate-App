42235a034c81526de32e33ff5d44c251ae9ee4b1
Sahil Patel <sahildepatel@gmail.com>
2022-08-11 18:51:11 -0400

    Fixing a bug with the popup

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,21 +7,20 @@ const Home = () => {
   const [showForm, setShowForm] = useState(false);
   const handleOnClose = () => setShowForm(false);
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f]">
+    <div name="home" className="w-full h-screen bg-gray-300">
       {/* container */}    
     <h1 className="text-[#8892b0] py-4 max-w[700px]">
     Already know what you are looking for?
     </h1>
-    <div className="bg-blue-400 bg-opacity-30">
+    
       <div className="max-w-3xl mx-auto">
         <div className="text-center py-3">
-          <button onClick={() => setShowForm(true)} className="bg-red-400 text-white px-3 py-2 rounded hover:scale-95 transition text-xl">
+          <button onClick={() => {setShowForm(true)}} className="bg-red-400 text-white mt-5 px-3 py-2 rounded hover:scale-95 transition text-xl">
             Get A free Estimate Now
           </button>
         </div>
-      </div>
     </div>
-    <Popup onCLose={handleOnClose} visible={showForm}/>
+    <Popup onCLose={handleOnClose} visible={showForm} handleForm={() => setShowForm(false)} />
   </div>
   );
 };
