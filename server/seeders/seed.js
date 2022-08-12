9ef773113af5e7e94a1ccb2a65a6fdd96eb979b6
RubenCastroCoding <RubenCastroCoding@gmail.com>
2022-08-11 21:06:57 -0400

    fixing issue

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,13 +1,16 @@
 const db = require("../config/connection");
-const { Estimate } = require("../models");
+const { Estimate,Profile } = require("../models");
 const estimateSeed = require("./estimateData.json");
+const profileSeed = require("./profileData.json");
 
 db.once("open", async () => {
   try {
     await Estimate.deleteMany({});
     await Estimate.create(estimateSeed);
+    await Profile.deleteMany({});
+    await Profile.create(profileSeed);
 
-    console.log("estimateDataSeeded!");
+    console.log("Profiles and Estimates Succesfully Seeded!");
     process.exit(0);
   } catch (err) {
     throw err;
